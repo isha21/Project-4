76d9bf9d230c798f4f1c2f3fe8e9e9abbe57a044
Ayesha Kanani <isha_21@yahoo.com>
2017-08-12 22:50:06 -0700

    added icon fonts to footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,8 +11,15 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="footer-container">
-					<div class="footer-block-left"
-							<h3>Contact Info</h3>
+					<div class="footer-text-container">
+						<div class="footer-text-left"
+								<h3>Contact Info</h3>
+								<p><i class="fa fa-envelope"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
+								<p><i class="fa fa-phone"></i><a href="tel:7784567891">778-456-7891</a></p>
+								<p><span><i class="fa fa-facebook-square"    aria-hidden="true"></i></span></p>
+								<p><span><i class="fa fa-twitter-square"     aria-hidden="true"></i></span></p>
+								<p><span><i class="fa fa-google-plus-square" aria-hidden="true"></i></span></p>
+
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
