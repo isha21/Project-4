88a257db821b26553073b80059d71f961fe359cd
Ayesha Kanani <isha_21@yahoo.com>
2017-08-12 19:21:19 -0700

    added classes and contact info to footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,7 +10,9 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
+				<div class="footer-container">
+					<div class="footer-block-left"
+							<h3>Contact Info</h3>
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
