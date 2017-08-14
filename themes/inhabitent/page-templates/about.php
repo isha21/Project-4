57b465d5093c94359b5580e67f0f7b65ddee5e2e
Ayesha Kanani <isha_21@yahoo.com>
2017-08-14 15:18:39 -0700

    added about page template

---
+++ themes/inhabitent/page-templates/about.php
@@ -0,0 +1,35 @@
+ <?php
+ 
+/**
+ * Template Name: About Page
+
+ */ 
+ 
+ get_header(); ?>
+
+<div id="primary"  class="content-area">
+    <main id="main" class="site-main"  role="main">
+
+<article id="post"-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-heder custom-hero">
+      <div class="container"
+        <>php the_title('h1 class="entry-title">', '</h1>'); ?>
+      </div>
+    </header>
+
+<div class="container">
+  <div class="enry-content">
+      <h2>Our Story</h2>  
+      <?php echo CFS()->get( '	about_our_story' ); ?>
+  
+      <h2> Our Team </h2>
+      <?php echo CFS()->get( '	about_our_team' ); ?>
+    </div>
+   </iv>
+  </article>
+
+
+    </main><!--#main -->
+  </div><!--#primary -->    
+
+<?php get_footer(); ?>
