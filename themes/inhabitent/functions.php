d567c8f56ea98f2b007ccb9c0d1b13faa21fc68d
Ayesha Kanani <isha_21@yahoo.com>
2017-08-14 16:02:06 -0700

    added hero image to about page

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -88,6 +88,7 @@ function inhabitent_scripts() {
 	wp_enqueue_script( 'inhabitent-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
   wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
+	wp_enqueue_style( 'tent-style', get_stylesheet_uri() );
 
 if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -7,26 +7,26 @@
  
  get_header(); ?>
 
-<div id="primary"  class="content-area">
+  <div id="primary"  class="content-area">
     <main id="main" class="site-main"  role="main">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-heder custom-hero">
-      <div class="container"
-        <?php the_title('h1 class="entry-title">', '</h1>'); ?>
-      </div>
-    </header>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header custom-hero">
+            <div class="container"
+               <?php the_title('h1 class="entry-title">', '</h1>'); ?>
+             </div>
+         </header>
 
-<div class="container">
-  <div class="enry-content">
-      <h2>Our Story</h2>  
-      <?php echo CFS()->get( 'about_our_story' ); ?>
+        <div class="container">
+           <div class="enry-content">
+             <h2>Our Story</h2>  
+             <?php echo CFS()->get( 'about_our_story' ); ?>
   
-      <h2> Our Team </h2>
-      <?php echo CFS()->get( 'about_our_team' ); ?>
-    </div>
-   </iv>
-  </article>
+             <h2> Our Team </h2>
+             <?php echo CFS()->get( 'about_our_team' ); ?>
+            </div>
+       </div>
+      </article>
 
 
     </main><!--#main -->
