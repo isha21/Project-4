a63959e9ba8af446f7a738b625a45b59c7c4b569
Ayesha Kanani <isha_21@yahoo.com>
2017-08-15 11:02:21 -0700

    fixed page template

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -22,23 +22,7 @@ get_header(); ?>
 ?>
 <?php get_header(); ?>
 
-<div class = "most-recent">
-		<?php 
-				$args = array ('post_type' => 'post', order => 'DESC', 'posts_per_page' => 3, 'orderby' => 'date');
-				$journal_posts = get_posts($args);
-		  ?>
-			<?php foreach ( $journal_posts as $post ) : setup_postdata($post); ?>
-				<div class ="journal-recent-block-item">
-					<div class ="journal-thumbnail-wrapper">
-						<?php if  ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail('medium'); ?>
-						<?php endif; ?>
-					</div>
-					<a href="<? echo get_post_permalink() ?>"> <?php the_title(); ?> </a>
-				</div>
 
-			<?php endforeach; wp_reset_postdata(); ?>
-</div>
 	
 
 		</main><!-- #main -->
