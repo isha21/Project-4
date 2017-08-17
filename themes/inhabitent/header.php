a749e49859d2065909eb9a585712209866708daf
Ayesha Kanani <isha_21@yahoo.com>
2017-08-16 23:49:57 -0700

    added search form to header

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -30,8 +30,14 @@
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-</div>
+				<span class="nav-search">
+					<?php get_search_form(); ?>
+				</span>
+				</div>
 				</nav><!-- #site-navigation -->
+			
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+
+			
