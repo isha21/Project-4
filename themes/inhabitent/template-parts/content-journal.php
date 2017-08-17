a7b84db53aad1c88a4cd5b3fb98a28f1adf2ac2d
Ayesha Kanani <isha_21@yahoo.com>
2017-08-17 16:15:59 -0700

    added template part content-journal to style home page

---
+++ themes/inhabitent/template-parts/content-journal.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ * Inherited from content-single
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+		<?php endif; ?>
+
+		<h1 class="journal-title"><a href="<?= get_the_permalink(); ?>"><?= get_the_title(); ?></a></h1>
+
+		<div class="entry-meta">
+			<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
