7e30b2000ae635788843b1fbb3d8f5a304ffa297
Ayesha Kanani <isha_21@yahoo.com>
2017-08-22 17:36:03 -0700

    added template part

--- themes/inhabitent/template-parts/content-shop.php
+++ themes/inhabitent/template-parts/content-shop.php
@@ -10,7 +10,8 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
+
+		<a href="<?php echo the_permalink();?>">	<?php the_post_thumbnail( 'large' );?></a>
 		<?php endif; ?>
 
 		<?php the_title( '<h2 class="entry-title">','</h2>' ); ?>
