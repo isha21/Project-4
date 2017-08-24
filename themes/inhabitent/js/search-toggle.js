70cf83a044be15a1c6e076e1c08743d597a8f68f
Ayesha Kanani <isha_21@yahoo.com>
2017-08-24 11:15:47 -0700

    worked on javascript

--- themes/inhabitent/js/search-toggle.js
+++ themes/inhabitent/js/search-toggle.js
@@ -3,5 +3,8 @@
     event.preventDefault();
     $('.icon-search').toggle('slow');
     $('.icon-search').focus();
-  })
+  });
+  $('.icon-search').blur(function(){
+  $('.icon-search').hide();
+  });
 });
