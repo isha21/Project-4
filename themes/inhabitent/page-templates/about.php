d54bbc483ecc970bdbea33296c7b0f1cf16d4403
Ayesha Kanani <isha_21@yahoo.com>
2017-08-29 21:57:55 -0700

    styled about page

--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -12,7 +12,7 @@
 
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="entry-header custom-hero">
-            <div class="container"
+        <div class="about-title"
                <?php the_title('h1 class="entry-title">', '</h1>'); ?>
              </div>
          </header>
